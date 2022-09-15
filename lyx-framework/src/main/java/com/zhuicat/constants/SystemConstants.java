b2a97dbdf8e946718a91a7a43a9ccaf2dc222a2b
huiqiulilili <5731303+huiqiulilili@user.noreply.gitee.com>
2022-09-15 18:34:58 +0800

    查询文章分类

--- lyx-framework/src/main/java/com/zhuicat/constants/SystemConstants.java
+++ lyx-framework/src/main/java/com/zhuicat/constants/SystemConstants.java
@@ -13,4 +13,10 @@ public class SystemConstants {
      *  文章是正常分布状态
      */
     public static final int ARTICLE_STATUS_NORMAL = 0;
+
+
+    /**
+     * 分类 是正常状态
+     */
+    public static final String STATUS_NORMAL = "0";
 }
---
+++ lyx-framework/src/main/java/com/zhuicat/domain/vo/CategoryVo.java
@@ -0,0 +1,12 @@
+package com.zhuicat.domain.vo;
+
+import lombok.Data;
+
+@Data
+public class CategoryVo {
+
+    private Long id;
+
+    //分类名
+    private String name;
+}
